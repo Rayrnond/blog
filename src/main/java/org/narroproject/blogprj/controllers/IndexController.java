5bd6b9de0c35e54d9230e745f31aeded53b6e4e8
Raymond <raymondpattend@icloud.com>
2023-10-22 21:50:53 -0400

    Minor modification to files to remove copyright duplication

--- src/main/java/org/narroproject/blogprj/controllers/IndexController.java
+++ src/main/java/org/narroproject/blogprj/controllers/IndexController.java
@@ -6,22 +6,6 @@
  *  © 2023 Raymond Pattend & Narro LLC. All rights reserved.
  */
 
-/*
- *  © 2023 Raymond Pattend & Narro LLC. All rights reserved.
- */
-
-/*
- *  © 2023 Raymond Pattend & Narro LLC. All rights reserved.
- */
-
-/*
- *  © 2023 Raymond Pattend & Narro LLC. All rights reserved.
- */
-
-/*
- *  © 2023 Raymond Pattend & Narro LLC. All rights reserved.
- */
-
 package org.narroproject.blogprj.controllers;
 
 import org.springframework.stereotype.Controller;
